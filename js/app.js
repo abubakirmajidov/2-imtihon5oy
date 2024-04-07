d8b352653a03a73bc92f7a841276a8c008f7f3e4
abubakirmajidov <abubakirmajidov@gmail.com>
2024-04-06 23:39:29 -1200

    imtihon5oy

---
+++ js/app.js
@@ -0,0 +1,38 @@
+const url = "https://api.adviceslip.com/advice";
+
+const ul = document.querySelector("div");
+const template = document.querySelector("template");
+const image = document.querySelector("img");
+const wrapper = document.querySelector(".wrapper");
+
+function getData() {
+  fetch(url)
+    .then((data) => {
+      return data.json();
+    })
+    .then((advice) => {
+      updateUl(advice);
+    })
+    .catch();
+}
+
+image.addEventListener("click", () => {
+  getData();
+});
+
+getData();
+
+function updateUl(data) {
+  ul.innerHTML = "";
+  console.log(data);
+  const clone = template.content.cloneNode(true);
+  const cards__id = clone.querySelector(".cards__id");
+  const cards__text = clone.querySelector(".cards__text");
+
+  cards__id.textContent = `Advice #${data.slip.id}`;
+  cards__text.textContent = `"${data.slip.advice}"`;
+
+  ul.appendChild(clone);
+  ul.appendChild(wrapper);
+  ul.appendChild(image);
+}
